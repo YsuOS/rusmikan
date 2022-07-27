788af4dd3cb4d29ac8e321b272521c3044716763
Ryosuke YASUOKA <yasuoka0830@gmail.com>
2022-07-27 17:12:49 +0900

    Fix FrameBufferConfig member: FrameBuffer -> original FrameBuffer

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
 #![no_std]
 
-use uefi::proto::console::gop::FrameBuffer;
-
 pub enum PixelFormat {
     RGB,
     BGR,
 }
 
-pub struct FrameBufferConfig<'gop> {
-    pub frame_buffer: FrameBuffer<'gop>,
+pub struct FrameBuffer {
+    pub base: *mut u8,
+}
+
+impl FrameBuffer {
+    pub unsafe fn write_value(&mut self, index: usize, val: [u8;3]) {
+        (self.base.add(index) as *mut [u8;3]).write_volatile(val)
+    }
+}
+
+pub struct FrameBufferConfig {
+    pub frame_buffer: FrameBuffer,
     pub pixels_per_scan_line: usize,
     pub horizontal_resolution: usize,
     pub vertical_resolution: usize,
