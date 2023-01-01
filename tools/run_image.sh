eb2f999e66bdb52d8733bc11c3f724008e67c3a2
Ryosuke Yasuoka <yasuoka0830@gmail.com>
2023-01-01 19:40:13 +0900

    add serial port device

--- tools/run_image.sh
+++ tools/run_image.sh
@@ -20,4 +20,5 @@ qemu-system-x86_64 \
     -drive if=pflash,format=raw,readonly,file=$DEVENV_DIR/OVMF_CODE.fd \
     -drive if=pflash,format=raw,file=$DEVENV_DIR/OVMF_VARS.fd \
     -drive if=ide,index=0,media=disk,format=raw,file=$DISK_IMG \
+    -serial stdio \
     $QEMU_OPTS
